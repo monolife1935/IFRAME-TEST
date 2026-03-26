fe19c0a72f2d4a58a09f2983592f5d67dffce99a
dorosee-mono <hc_yang@dorosee.kr>
2026-03-26 13:16:39 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import "./App.css";
 
 function App() {
+  const userId = "dsaccsa";
   return (
     <>
       <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+        <div
+          style={{
+            display: "flex",
+            flexFlow: "column nowrap",
+            alignItems: "start",
+          }}
+        >
+          <p style={{ fontSize: "12px" }}>
+            클럽포털 {">"} 어쩌구 {">"} 통합회원
           </p>
+          <h1>통합회원 </h1>
+          <p style={{ marginBottom: "12px" }}>클럽 포털 통합회원 어쩍구 현황</p>
           <iframe
-            width="900px"
-            height="600px"
-            src="https://cluster.syncbox.co.kr/#/public/dashboard/1b466b75-a844-470d-a1cf-14ae979250f3"
+            width="720px"
+            height="405px"
+            src={`http://localhost:39050/#/report/publish/5f9cba01-5254-449c-bb46-40789793b15f?embed=true`}
+            frameBorder={0}
+            allowFullScreen={true}
           ></iframe>
         </div>
       </section>
